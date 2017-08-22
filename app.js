8167e91871b178bec2655e04624952762b5ea6a7
saulobr88 <saulobr88@yahoo.com.br>
2017-08-22 20:50:44 -0300

    Data, Method & v-bind introduction

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+  el:'#vue-app',
+  data: {
+    name: 'Saulo',
+    job: 'Developer',
+    website: 'https://github.com',
+    websiteTag: '<a href="https://github.com/saulobr88" target="_blank">Website</a>'
+  },
+  methods: {
+    greet: function(time){
+        return 'Good ' + time +' '+ this.name;
+    }
+  }
+});
